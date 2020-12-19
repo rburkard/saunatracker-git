33ec08c1e21eb80c7620853db9334a1d70095b51
Roman Burkard <romanburkard@Romans-MacBook-Pro.local>
2020-12-19 11:40:06 +0100

    adding view url

--- saunatracker/trackerapp/urls.py
+++ saunatracker/trackerapp/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('polls/', include('trackerapp.urls')),
+    path('trackerapp/', include('trackerapp.urls')),
     path('admin/', admin.site.urls),
 ]
