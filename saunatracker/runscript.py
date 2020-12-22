1a7da3212417d5d7669dce0064a5a26e812d6c8f
Roman Burkard <romanburkard@Romans-MacBook-Pro.local>
2020-12-22 14:10:55 +0100

    release hourly script

--- saunatracker/runscript.py
+++ saunatracker/runscript.py
@@ -16,24 +16,20 @@ import time
 import datetime
 from bs4 import BeautifulSoup
 
-while True:
+# creating object 
+current_time = datetime.datetime.today().timetuple()
 
-    # creating object 
-    current_time = datetime.datetime.today().timetuple()
-    
-    # displaying the tuples of the object
-    if current_time.tm_hour >= 8 and current_time.tm_hour < 20:
+# displaying the tuples of the object
+if current_time.tm_hour >= 8 and current_time.tm_hour < 20:
+    for i in range(4):
         url = 'https://www.seebadenge.ch/wp/sauna'
         response = requests.get(url)
         print(response)
+        print(current_time.tm_hour)
 
         soup = BeautifulSoup(response.text, "html.parser")
         c_count = soup.findAll('span')[-3].text
 
         print(c_count)
         Track(count = c_count).save()
-        time.sleep(600)
-    
-    else:
-        time.sleep(60)
-        continue
+        time.sleep(900)
